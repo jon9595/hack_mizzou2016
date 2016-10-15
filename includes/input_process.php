4419db0593fc24e455c7552af83397a1e06bda6c
Jon <jon9595@gmail.com>
2016-10-15 00:03:20 -0500

    final commit on this branch

--- includes/input_process.php
+++ includes/input_process.php
@@ -1,3 +1,5 @@
 <?php
   $verb = $_SERVER['REQUEST_METHOD'];
+  $request = file_get_contents('php://input'); //reading in the passed json then decoding it on the next line
+  $arr = json_decode($request);
 ?>
